84119c3885f2cc8c030cd56a3fea33fd128d9872
Suryaansh-Jain <suryaansh2003@gmail.com>
2023-04-05 23:34:57 +0530

    added the post api

--- server/server/main.py
+++ server/server/main.py
@@ -1,15 +1,22 @@
 import os
+import time
 from typing import Union
 
 import psycopg2
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Header, HTTPException
 from psycopg2.extras import Json, RealDictCursor
+from pydantic import BaseModel
 import aiosql
+import json
 
 from auth import get_user_email
 
 load_dotenv()
+
+COLS = 100
+ROWS = 100
+COLORS = 32
 
 DATABASE = os.getenv("DATABASE")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
@@ -17,7 +24,9 @@ POSTGRES_PASS = os.getenv("POSTGRES_PASS")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
+
 app = FastAPI()
+queries = aiosql.from_path("./queries.sql", "psycopg2")
 
 conn = psycopg2.connect(
     database=DATABASE,
@@ -51,6 +60,13 @@ def verify_auth_token(Authorization: str = Header()):
         )
     return email
 
+def get_user_cooldown(email: str):
+    prev_req_time = queries.get_last_update_by_user(conn, email)
+    
+    if (float(time.time()) - float(prev_req_time)) < 600:
+        return False
+    
+    return True
 
 @app.get("/")
 def read_root():
@@ -64,6 +80,24 @@ async def auth(email: str = Depends(verify_auth_token)):
     """
     return {"email": email}
 
+@app.post("/pixel/{row}/{col}/{color}}")
+async def pixel(x: int, y: int, color: int, email: str = Depends(verify_auth_token)):
+    if (x < 0 or x >= ROWS or y < 0 or y >= COLS or color < 0 or color >= COLORS):
+        raise HTTPException(
+            status_code=400, detail="Invalid pixel coordinates or color."
+        )
+
+    if not get_user_cooldown(email):
+        raise HTTPException(
+            status_code=429, detail="You are on cooldown."
+        )
+    
+
+    queries.log_update(conn, email, x, y, color)
+
+    raise HTTPException(
+        status_code=200, detail="Pixel updated."
+    )
 
 @app.get("/full_grid")
 def full_grid():
