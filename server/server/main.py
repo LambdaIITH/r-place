59fd87e8f7caad647d8ef0bbb3b22a2ef8aca7ec
Rachit Keerti Das <rachitkdas@gmail.com>
2023-04-05 11:05:47 +0530

    Connect to DB, load creds from env
    add aiosql and pypika to deps for further query creation

--- server/server/main.py
+++ server/server/main.py
@@ -1,8 +1,32 @@
+import os
 from typing import Union
+
+import psycopg2
+from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Header, HTTPException
+from psycopg2.extras import Json, RealDictCursor
+
 from auth import get_user_email
 
+load_dotenv()
+
+DATABASE = os.getenv("DATABASE")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASS = os.getenv("POSTGRES_PASS")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
 app = FastAPI()
+
+conn = psycopg2.connect(
+    database=DATABASE,
+    user=POSTGRES_USER,
+    password=POSTGRES_PASS,
+    host=POSTGRES_HOST,
+    port=POSTGRES_PORT,
+    cursor_factory=RealDictCursor,
+)
+print("Opened database successfully!")
 
 
 def verify_auth_token(Authorization: str = Header()):
