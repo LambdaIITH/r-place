3113f000ff1bebc33cd8a254f21b9822e697e63b
kst164 <kartheek@tammana.org>
2023-04-05 23:12:41 +0530

    Backend: added /full_grid

--- server/server/main.py
+++ server/server/main.py
@@ -5,6 +5,7 @@ import psycopg2
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Header, HTTPException
 from psycopg2.extras import Json, RealDictCursor
+import aiosql
 
 from auth import get_user_email
 
@@ -24,9 +25,22 @@ conn = psycopg2.connect(
     password=POSTGRES_PASS,
     host=POSTGRES_HOST,
     port=POSTGRES_PORT,
-    cursor_factory=RealDictCursor,
 )
 print("Opened database successfully!")
+
+queries = aiosql.from_path("queries.sql", "psycopg2")
+
+ROWS = 100
+COLUMNS = 100
+COLORS = 32
+
+INITIAL_COLOR = 0
+
+current_grid = [[INITIAL_COLOR for _ in range(COLUMNS)] for _ in range(ROWS)]
+# initialize the in-mem grid
+for x, y, color in queries.get_full_grid(conn):
+    print(x, y, color)
+    current_grid[x][y] = color
 
 
 def verify_auth_token(Authorization: str = Header()):
@@ -49,3 +63,11 @@ async def auth(email: str = Depends(verify_auth_token)):
     Test Endpoint to validate user identity.
     """
     return {"email": email}
+
+
+@app.get("/full_grid")
+def full_grid():
+    """
+    Return the full grid.
+    """
+    return current_grid
