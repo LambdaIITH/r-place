f939eb9ec3d34a454cd33d0297d211dc4338be62
Rachit Keerti Das <rachitkdas@gmail.com>
2023-06-29 00:14:37 +0530

    [WIP] Add textarea and fetch call for post comment
    Need to add:
    viewing component,
    validate current user
    load existing component
    add edit/delete calls

--- client/pages/hostels/[hostel]/[floor]/[room].js
+++ client/pages/hostels/[hostel]/[floor]/[room].js
@@ -1,42 +1,55 @@
 import { useRouter } from 'next/router'
 import { Carousel } from '@mantine/carousel'
-import { useMantineTheme, Title, createStyles, Text } from '@mantine/core'
+import {
+  Title,
+  createStyles,
+  Text,
+  Textarea,
+  Container,
+  Button,
+} from '@mantine/core'
 import { useEffect, useState } from 'react'
 import Layout from '../../../../components/layouts/hostel_layout'
 const useStyles = createStyles((theme) => ({
-  carousel:{
-
-  },
-  slide:{
+  carousel: {},
+  slide: {
     padding: '1rem',
     backgroundColor: theme.colors.blue[0],
   },
-  question:{
-
-  },
-  answer:{
-    
-  }
-}));
-
+  question: {},
+  answer: {},
+}))
 
 export default function Room() {
   const router = useRouter()
   const { hostel, floor, room } = router.query
-  const theme = useMantineTheme()
   const [questions, setQuestions] = useState([])
   const [answers, setAnswers] = useState([])
-  const { classes, cx } = useStyles();
+  const { classes, cx } = useStyles()
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [loading, setLoading] = useState(true)
+  const [commentValue, setCommentValue] = useState('')
+
+  const postComment = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/hostel/${hostel}/${floor}/${room}/comments`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ comment: commentValue }),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+    if (res.status == 200) {
+      console.log('Comment Posted Successfuly')
+    } else {
+      console.log('Some errors occured')
+    }
+  }
 
   async function getRoomData() {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/hostel/${hostel}/${floor}/${room}`,
-      {
-        method: 'GET',
-      }
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/hostel/${hostel}/${floor}/${room}`
     )
     if (res.status == 400 || res.status == 404) {
       // router.push('/404')
@@ -62,35 +75,47 @@ export default function Room() {
   }, [router.isReady])
   return (
     <>
-    {loading ? <></>:
-      <>
-      <Title align="center" mt={12} mb={24}>
-        Welcome to {name}'s room!
-      </Title>
-      <Carousel
-        maw={320}
-        mx="auto"
-        withIndicators
-        height={200}
-        slideGap="md"
-        align="start"
-        loop
-        className={classes.carousel}
-      >
-        {questions.map((question, index) => (
-          <Carousel.Slide key={index} className={classes.slide}>
-            <Text className={classes.question}>
-            {question} :
-            </Text>
-            <Text className={classes.answer}>
-            {answers[index]}
-            </Text>
-             
-          </Carousel.Slide>
-        ))}
-      </Carousel>
-      </>
-    }
+      {loading ? (
+        <></>
+      ) : (
+        <Container>
+          <Title align="center" mt={12} mb={24}>
+            Welcome to {name}'s room!
+          </Title>
+          <Carousel
+            maw={320}
+            mx="auto"
+            withIndicators
+            height={200}
+            slideGap="md"
+            align="start"
+            loop
+            className={classes.carousel}
+          >
+            {questions.map((question, index) => (
+              <Carousel.Slide key={index} className={classes.slide}>
+                <Text className={classes.question}>{question} :</Text>
+                <Text className={classes.answer}>{answers[index]}</Text>
+              </Carousel.Slide>
+            ))}
+          </Carousel>
+
+          <Textarea
+            placeholder="Type your mind"
+            label="Leave a comment"
+            description="Comments are only visible to the room owner"
+            radius="md"
+            mx={16}
+            value={commentValue}
+            onChange={(event) => {
+              setCommentValue(event.target.value)
+            }}
+          />
+          <Button my={12} mx={16} onClick={postComment}>
+            Post Comment
+          </Button>
+        </Container>
+      )}
     </>
   )
 }
